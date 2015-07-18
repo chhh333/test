ce28b1af6979e75f3a614ad3bc6b5ba5eb9c4b2d
chhh333 <chhh333@gmail.com>
2015-07-18 15:01:24 +0900

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -1,3 +1,13 @@
-public class factorial() {
-
+public class factorial(){
+  public static int fact(int num){
+    if(num==1){
+      return 1;
+    }else {
+      return num*fact(num-1);
+    }
+  }
+  
+  public static void main(String[] args){
+    System.out.print(fact(5));
+  }
 }
